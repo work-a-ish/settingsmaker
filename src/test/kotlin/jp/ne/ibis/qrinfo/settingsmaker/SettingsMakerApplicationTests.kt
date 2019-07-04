f6ca2666b445e5ee1323aaccc486abc6bbf277b7
ishii <a-ishi@ibis.ne.jp>
2019-07-04 14:46:26 +0900

    first commit

---
+++ src/test/kotlin/jp/ne/ibis/qrinfo/settingsmaker/SettingsMakerApplicationTests.kt
@@ -0,0 +1,16 @@
+package jp.ne.ibis.qrinfo.settingsmaker
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SettingsMakerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
